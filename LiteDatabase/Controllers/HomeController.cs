0d1e967addc2a096b83d3567c177b5e4af98c75f
Richard <Richard@DESKTOP-9LM3OJE>
2020-03-10 21:44:20 -0300

    Criação de projeto simples utilizando LiteDatabase. Projeto Web em Net Core 3.1

--- LiteDatabase/Controllers/HomeController.cs
+++ LiteDatabase/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using LiteDatabase.Models;
+using LiteDatabase.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using LiteDatabase.Models;
+using System;
+using System.Diagnostics;
 
 namespace LiteDatabase.Controllers
 {
@@ -20,7 +18,33 @@ namespace LiteDatabase.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(new DataBase().Listar());
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario user)
+        {
+            new DataBase().Inserir(user);
+            TempData["Msg"] =  user.Nome + " foi cadastrado com sucesso!";
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Alterar(Usuario user)
+        {
+            new DataBase().Alterar(user);
+            TempData["Msg"] = user.Nome + " foi alterado com sucesso!";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Excluir(Guid id)
+        {
+            var busca = new DataBase().BuscaPorId(id);
+            new DataBase().Excluir(id);
+            TempData["Msg"] = busca.Nome + " foi excluído com sucesso!";
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Alterar(Guid id)
+        {
+            return View(new DataBase().BuscaPorId(id));
         }
 
         public IActionResult Privacy()
---
+++ LiteDatabase/Services/DataBase.cs
@@ -0,0 +1,60 @@
+﻿using LiteDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LiteDatabase.Services
+{
+    public class DataBase
+    {
+        public bool Inserir(Usuario newUser)
+        {
+            using (var db = new LiteDB.LiteDatabase("bancoDeDados.db"))
+            {
+                newUser.Id = Guid.NewGuid();
+                var userCollection = db.GetCollection<Usuario>("usuarios");
+                userCollection.Insert(newUser);
+                return true;
+            }
+        }
+        public List<Usuario> Listar()
+        {
+            using (var db = new LiteDB.LiteDatabase("bancoDeDados.db"))
+            {
+                var usersCollection = db.GetCollection<Usuario>("usuarios");
+                return usersCollection.FindAll().ToList();
+            }
+        }
+        public bool Excluir(Guid id)
+        {
+            using (var db = new LiteDB.LiteDatabase("bancoDeDados.db"))
+            {
+                var usersCollection = db.GetCollection<Usuario>("usuarios");
+                return usersCollection.Delete(id);
+            }
+        }
+        public Usuario BuscaPorId(Guid id)
+        {
+            using (var db = new LiteDB.LiteDatabase("bancoDeDados.db"))
+            {
+                var usersCollection = db.GetCollection<Usuario>("usuarios");
+                return usersCollection.FindOne(x => x.Id == id);
+            }
+        }
+        public bool Alterar(Usuario user)
+        {
+            using (var db = new LiteDB.LiteDatabase("bancoDeDados.db"))
+            {
+                var usersCollection = db.GetCollection<Usuario>("usuarios");
+                var usuario = usersCollection.FindOne(x => x.Id == user.Id);
+
+                if (!string.IsNullOrWhiteSpace(user.Nome))
+                    usuario.Nome = user.Nome;
+                if (!string.IsNullOrWhiteSpace(user.Senha))
+                    usuario.Senha = user.Senha;
+
+                return usersCollection.Update(usuario);
+            }
+        }
+    }
+}
